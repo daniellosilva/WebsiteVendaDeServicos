39db18062651f6af3e847b2ced65b6579f113e8b
kauancafe <kauancavalcantef@gmail.com>
2025-02-22 23:09:25 -0300

    kamikaze

--- controllers/servicos.controllers.js
+++ controllers/servicos.controllers.js
@@ -1,11 +1,15 @@
-import { Router } from "express"
-import { db } from "../database/db.js"
+import { Router } from "express";
+import { db } from "../database/db.js";
 
-async function obterServicos (req, res) {
-    const filmes = await db.all("SELECT * FROM servicos")
-    return res.status(200).json(filmes)
+async function obterServicos(req, res) {
+    try {
+        const servicos = await db.all("SELECT * FROM servicos");
+        return res.status(200).json(servicos);
+    } catch (error) {
+        return res.status(500).json({ error: "Erro ao buscar serviços" });
+    }
 }
 
-export {
+export { 
     obterServicos,
 }
