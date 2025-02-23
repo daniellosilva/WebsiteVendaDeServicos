ca89bfd08639e0fd26b4499516813b8d2480a343
daniellosilva <dl238611@gmail.com>
2025-02-22 21:11:52 -0300

    cadastro

--- controllers/cadastro.js
+++ controllers/cadastro.js
@@ -27,33 +27,5 @@ document.getElementById('form-cadastro').addEventListener('submit', function (ev
       return;
     }
   
-    // Cria o objeto com os dados do formulário
-    const dados = {
-      nome,
-      sobrenome,
-      email,
-      senha,
-    };
-  
-    // Envia os dados para o backend
-    fetch('http://localhost:5000/api/cadastro', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(dados),
-    })
-      .then(response => response.json())
-      .then(data => {
-        if (data.message) {
-          alert(data.message); // Exibe mensagem de sucesso
-          window.location.href = 'login.html'; // Redireciona para a página de login
-        } else if (data.error) {
-          alert(data.error); // Exibe mensagem de erro
-        }
-      })
-      .catch(error => {
-        console.error('Erro ao enviar dados:', error);
-        alert('Erro ao conectar com o servidor.');
-      });
+    
   });
